fc234f88672a1ff5e1e0482f1af388df1beedeb7
sania <sania@cloudkibo.com>
2019-03-01 19:46:13 +0500

    Environment Variables

---
+++ server/config/environment/staging.js
@@ -0,0 +1,37 @@
+'use strict'
+
+// Staging specific configuration
+// ==================================
+module.exports = {
+
+  // Server port
+  port: process.env.PORT || 3000,
+
+  // Secure Server port
+  secure_port: process.env.SECURE_PORT || 8443,
+
+  domain: `${process.env.DOMAIN || 'https://skibolite.cloudkibo.com'}`,
+
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/kibolite-staging'
+  },
+  seedDB: false,
+  facebook: {
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: `${process.env.DOMAIN}/auth/facebook/callback`
+  },
+
+  api_urls: {
+    webhook: 'https://swebhook.cloudkibo.com/api',
+    kibopush: 'https://staging.kibopush.com/api',
+    accounts: 'https://saccounts.cloudkibo.com/api/v1',
+    chat: 'https://skibochat.cloudkibo.com/api',
+    kibochat: `${process.env.DB_LAYER_IP_KIBOCHAT}/api/v1`,
+    kiboengage: `http://skiboengage.cloudkibo.com/api`
+    // kiboengage: `${process.env.DB_LAYER_IP_KIBOENGAGE}/api/v1`
+  },
+  webhook_ip: process.env.WEBHOOK_IP_ADDRESS || 'localhost'
+
+}
