eb2fe844f9d80f138ad243d15bd04a732cd22f95
Arveen kumar <arveen@cloudkibo.com>
2020-03-27 15:29:43 +0500

    hot_fix

--- server/api/v1/businessGateway/customer.controller.js
+++ server/api/v1/businessGateway/customer.controller.js
@@ -271,7 +271,7 @@ function getBatchData (payload, fbMessageTag, sendOption, page, reject, resolve)
           reject('fail')
           return logger.serverLog(TAG, `Batch send error ${JSON.stringify(err)}`)
         }
-        let resp = JSON.stringify(body[0].body)
+        let resp = JSON.parse(body[0].body)
         console.log('response from facebook withour stringyfy', body[0].body)
         console.log('response from facebook stringyfy', resp)
         if (resp.error && resp.error.message) {
